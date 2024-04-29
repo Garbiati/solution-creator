443dcd9258cf83c0eef218e47043e2c59e454660
Alessandro Henrique Garbiati <a.garbiati@gmail.com>
2024-04-29 09:09:24 -0300

    Refactored Dockerfile.live, Dockerfile, and docker-compose.yml to use PROJECT_NAME variable instead of hardcoding the project name

--- dotnet/api/scripts/configure-docker.sh
+++ dotnet/api/scripts/configure-docker.sh
@@ -3,11 +3,27 @@
 # Ensure the Dockerfile template exists using the defined path
 validate_file "$DOCKERFILE_TEMPLATE_PATH"
 
-# Path to the new Dockerfile within the .Api directory
+validate_file "$DOCKERFILE_LIVE_TEMPLATE_PATH"
+
+validate_file "$DOCKER_COMPOSE_TEMPLATE_PATH"
+
+# Path to the new Dockerfile
 NEW_DOCKERFILE="$PROJECT_NAME.Api/Dockerfile"
+
+NEW_DOCKERFILE_LIVE="$PROJECT_NAME.Api/Dockerfile.live"
+
+NEW_DOCKER_COMPOSE="docker-compose.yml"
 
 # Use 'sed' to replace placeholders with the project name
 sed "s/{{PROJECT_NAME}}/$PROJECT_NAME/g" "$DOCKERFILE_TEMPLATE_PATH" > "$NEW_DOCKERFILE"
 
+sed "s/{{PROJECT_NAME}}/$PROJECT_NAME/g" "$DOCKERFILE_LIVE_TEMPLATE_PATH" > "$NEW_DOCKERFILE_LIVE"
+
+sed "s/{{PROJECT_NAME}}/$PROJECT_NAME/g" "$DOCKER_COMPOSE_TEMPLATE_PATH" > "$NEW_DOCKER_COMPOSE"
+
 # Confirm successful creation
 validate_file "$NEW_DOCKERFILE"
+
+validate_file "$NEW_DOCKERFILE_LIVE"
+
+validate_file "$NEW_DOCKER_COMPOSE"
