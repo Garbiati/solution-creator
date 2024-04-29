7e9a6484db6534c7e255337996267436168b4dfb
Alessandro Henrique Garbiati <a.garbiati@gmail.com>
2024-04-29 12:42:34 -0300

    Refactor project structure to create additional directories for Domain, Application, and Infra.Data

--- dotnet/api/scripts/create-structures.sh
+++ dotnet/api/scripts/create-structures.sh
@@ -9,7 +9,25 @@ rm "$PROJECT_NAME.Api/$PROJECT_NAME.Api.http"
 print_info "Removed automatically created Class1.cs files."
 
 # Create basic structures for Domain, Application, and Infra.Data
-mkdir "$PROJECT_NAME.Domain/Entities" "$PROJECT_NAME.Domain/Interfaces" "$PROJECT_NAME.Domain/Validation"
-mkdir "$PROJECT_NAME.Application/ViewModels" "$PROJECT_NAME.Application/Interfaces" "$PROJECT_NAME.Application/Mappings" "$PROJECT_NAME.Application/Services"
-mkdir "$PROJECT_NAME.Infra.Data/Context" "$PROJECT_NAME.Infra.Data/EntitiesConfiguration" "$PROJECT_NAME.Infra.Data/Repositories"
+mkdir "$PROJECT_NAME.Domain/Aggregates"
+mkdir "$PROJECT_NAME.Domain/Entities"
+mkdir "$PROJECT_NAME.Domain/Enum"
+mkdir "$PROJECT_NAME.Domain/Interfaces"
+mkdir "$PROJECT_NAME.Domain/Validation"
+
+mkdir "$PROJECT_NAME.Application/Interfaces"
+mkdir "$PROJECT_NAME.Application/Mappings"
+mkdir "$PROJECT_NAME.Application/Services"
+mkdir "$PROJECT_NAME.Application/ViewModels"
+
+
+mkdir "$PROJECT_NAME.Infra.Data/Context"
+mkdir "$PROJECT_NAME.Infra.Data/EntitiesConfiguration"
+mkdir "$PROJECT_NAME.Infra.Data/Helpers/Data"
+mkdir "$PROJECT_NAME.Infra.Data/Repositories"
+
+mkdir -p "$PROJECT_NAME.Api/Controllers/Shared"
+mkdir -p "$PROJECT_NAME.Api/Controllers/v1"
+mkdir "$PROJECT_NAME.Api/Extensions"
+
 print_info "Created basic structures for Domain, Application, and Infra.Data."
