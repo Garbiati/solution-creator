08ccd506394e50b9f959c0c4c5be2a3807b7b3c7
Alessandro Henrique Garbiati <a.garbiati@gmail.com>
2024-10-20 13:40:34 -0300

    Refactor ExampleViewModel and PagedResult classes

--- dotnet/api/templates/base/Application/ViewModels/PagedResult.cs
+++
@@ -1,9 +0,0 @@
-namespace {{SOLUTION_NAME}}.Application.ViewModels;
-public class PagedResult<T>
-{
-    public IEnumerable<T> Items { get; set; } = Enumerable.Empty<T>();
-    public int TotalCount { get; set; }
-    public int TotalPages { get; set; }
-    public int CurrentPage { get; set; }
-    public int PageSize { get; set; }
-}
